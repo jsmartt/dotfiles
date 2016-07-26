1f5d07739be937a5dce2cfcc8881d342d199a70b
Jared Smartt <jsmartt@users.noreply.github.com>
2016-07-26 08:26:06 -0600

    Create proxy.sh

---
+++ proxy.sh
@@ -0,0 +1,7 @@
+PROXY="http://proxy.houston.hpecorp.net:8080"
+export HTTP_PROXY=$PROXY
+export http_proxy=$PROXY
+export HTTPS_PROXY=$PROXY
+export https_proxy=$PROXY
+export no_proxy="localhost,127.0.0.1,/var/run/docker.sock"
+export NO_PROXY="localhost,127.0.0.1,/var/run/docker.sock"
